68c3a44d34676d376cca51f43ac682300db3eb7a
Wesley Campbell <wcamp64@byu.edu>
2026-01-29 19:34:39 -0700

    REFACTOR: refactored the wheel speed frequency calculation algorithm.

--- src/wheels/speed.cpp
+++ src/wheels/speed.cpp
@@ -1,6 +1,6 @@
 #include "speed.h"
 
-static uint8_t lastHighLowRead = 0;
+static uint8_t lastRead = 0;
 static uint32_t flowRateTimer = 0;
 static uint16_t numRises = 0;
 static uint8_t speedPin = 0;
@@ -15,38 +15,38 @@ void speedISR(){
 }
 
 void calculateSpeed() {
-    if (!checkSpeed) return;
-    //Serial.println("CHECKING SPEED");
-    uint8_t highLowRead = (analogRead(speedPin) > 300);
-    //if (highLowRead) Serial.println("SENSOR HIGH");
-    //if (highLowRead) { Serial.println("DEBUG: SENSOR PIN IS HIGH"); }
-    //check for no change and then do nothing
-    if(highLowRead == lastHighLowRead){
-        ++flowRateTimer;
-        return;
-    } else {
-        lastHighLowRead = highLowRead;
-        if(highLowRead) {
-            /*counting the number of time we are riseing
-            or low to high pulses on the sensor
-            Note that the sensor is NOT PWM it is changeing frequncy
-            in stead*/
-            ++numRises;
-        }
+	// Only operates when checkSpeed flag is true
+	if (!checkSpeed) { return; }
 
-        if(numRises < SAMPLE_COUNT){
-            //do nothing if we dont have enough data points
-            return;
-        }
-        
-        if(flowRateTimer > 0){//devide my 0 protection
-            readFrequency = SAMPLE_FREQUENCY_HZ / flowRateTimer;
-        }
-        flowRateTimer = 0;
-        numRises = 0;
-        checkSpeed = false;
-    }
-}
+	// check to see if the current reading is high
+	uint8_t currentRead = (analogRead(speedPin) > 300);
+
+	if (currentRead == lastRead) {
+		++flowRateTimer;
+		return;
+	} else {
+		// Set the previous read value to the curretn
+		lastRead = currentRead;
+
+		if (currentRead == true) {
+			++numRises;
+		}
+
+		// wait until we have enough data samples
+		if (numRises < SAMPLE_COUNT) { 
+			return;
+		}
+
+		if(flowRateTimer > 0) {  // protects against divide by 0
+			readFrequency = SAMPLE_FREQUENCY_HZ / flowRateTimer;						
+		}
+
+		flowRateTimer = 0;
+		numRises = 0;
+		// reset the flag
+		checkSpeed = false;
+	}
+}	
 
 float getSpeed(){
     calculateSpeed();
